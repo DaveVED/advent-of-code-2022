ebaa012f944feaa84327ccafbf1009267e24c185
Dave Dennis <dave.w.dennis@gmail.com>
2022-12-14 17:06:51 -0800

    day2 tests

--- aoc/day2/rock_paper_scissors.py
+++ aoc/day2/rock_paper_scissors.py
@@ -1,7 +1,17 @@
 from typing import List
 
 
-def match_outcome_score(opponenet_move: str, outcome: str) -> int:
+def match_outcome_score(opponent_move: str, outcome: str) -> int:
+    """
+    Calculate the score for a match outcome.
+
+    Args:
+        opponent_move: The move made by the opponent, as a string ("A", "B", or "C").
+        outcome: The outcome of the match, as a string ("X", "Y", or "Z").
+
+    Returns:
+        The score for the match outcome, as an integer.
+    """
     match_outcome_map = {"X": 0, "Y": 3, "Z": 6}
     move_value_map = {"X": 1, "Y": 2, "Z": 3}
     matchups_map = {
@@ -10,34 +20,52 @@ def match_outcome_score(opponenet_move: str, outcome: str) -> int:
         "Z": {"A": "Y", "B": "Z", "C": "X"},
     }
 
-    return move_value_map[matchups_map[outcome][opponenet_move]] + match_outcome_map[outcome]
+    return move_value_map[matchups_map[outcome][opponent_move]] + match_outcome_map[outcome]
 
 
-def guess_outcome_score(opponenet_move: str, our_move: str) -> int:
+def guess_outcome_score(opponent_move: str, our_move: str) -> int:
+    """
+    Calculate the score for a guessed match outcome.
+
+    Args:
+        opponent_move: The move made by the opponent, as a string ("A", "B", or "C").
+        our_move: The move made by us, as a string ("X", "Y", or "Z").
+
+    Returns:
+        The score for the guessed match outcome, as an integer.
+    """
     win_map = {"X": "C", "Y": "A", "Z": "B"}
     tie_map = {"X": "A", "Y": "B", "Z": "C"}
     choice_map = {"X": 1, "Y": 2, "Z": 3}
     round_outcome_score = 0
-    if win_map[our_move] == opponenet_move:
+    if win_map[our_move] == opponent_move:
         round_outcome_score = 6
-    elif tie_map[our_move] == opponenet_move:
+    elif tie_map[our_move] == opponent_move:
         round_outcome_score = 3
     return round_outcome_score + choice_map[our_move]
 
 
-def calculate_rock_paper_scissors_score(file_path: str, part: bool) -> int:
+def calculate_rock_paper_scissors_score(file_path: str, part: str) -> int:
+    """
+    Calculate the total score for a game of Rock-Paper-Scissors.
+
+    Args:
+        file_path: The path to the file containing the game moves, as a string.
+        part: The part of the game to calculate the score for, as a string ("one" or "two").
+
+    Returns:
+        The total score for the game, as an integer.
+    """
     with open(file_path, "r") as file:
         lines = file.readlines()
 
         scores = []
-        for index, line in enumerate(lines):
-            if part:
-                opponenet_move, outcome = tuple(line.strip().split(" "))
-                match_score = match_outcome_score(opponenet_move, outcome)
-                scores.append(match_score)
-            else:
-                opponenet_move, outcome = tuple(line.strip().split(" "))
-                match_score = guess_outcome_score(opponenet_move, outcome)
-                scores.append(match_score)
+        for line in lines:
+            opponent_move, outcome = tuple(line.strip().split(" "))
+            if part == "two":
+                match_score = match_outcome_score(opponent_move, outcome)
+            elif part == "one":
+                match_score = guess_outcome_score(opponent_move, outcome)
+            scores.append(match_score)
 
     return sum(scores)
--- test/test_rock_paper_scissors.py
+++ test/test_rock_paper_scissors.py
@@ -1,25 +1,60 @@
 import pytest
+import aoc.day2.rock_paper_scissors as rock_paper_scissors
+
+from typing import List
+
+TOTAL_SCORE_PART_1_EASY = 15
+TOTAL_SCORE_PART_1_HARD = 14163
+TOTAL_SCORE_PART_2_EASY = 12
+TOTAL_SCORE_PART_2_HARD = 12091
 
 
 def test_match_outcome_score():
-    # Test input with a easy calorie file.
-    assert True
+    """Test the `match_outcome_score` function with a variety of input values."""
+    # Test matching outcome 'X'
+    assert rock_paper_scissors.match_outcome_score("A", "X") == 3
+    assert rock_paper_scissors.match_outcome_score("B", "X") == 1
+    assert rock_paper_scissors.match_outcome_score("C", "X") == 2
 
-    # Test input with mian calorie file
-    assert True
+    # Test matching outcome 'Y'
+    assert rock_paper_scissors.match_outcome_score("A", "Y") == 4
+    assert rock_paper_scissors.match_outcome_score("B", "Y") == 5
+    assert rock_paper_scissors.match_outcome_score("C", "Y") == 6
+
+    # Test matching outcome 'Z'
+    assert rock_paper_scissors.match_outcome_score("A", "Z") == 8
+    assert rock_paper_scissors.match_outcome_score("B", "Z") == 9
+    assert rock_paper_scissors.match_outcome_score("C", "Z") == 7
 
 
 def test_guess_outcome_score():
-    # Test input with a easy calorie file.
-    assert True
+    """Test the `guess_outcome_score` function with a variety of input values."""
+    # Test guessing outcome 'X'
+    assert rock_paper_scissors.guess_outcome_score("A", "X") == 4
+    assert rock_paper_scissors.guess_outcome_score("B", "X") == 1
+    assert rock_paper_scissors.guess_outcome_score("C", "X") == 7
 
-    # Test input with mian calorie file
-    assert True
+    # Test guessing outcome 'Y'
+    assert rock_paper_scissors.guess_outcome_score("A", "Y") == 8
+    assert rock_paper_scissors.guess_outcome_score("B", "Y") == 5
+    assert rock_paper_scissors.guess_outcome_score("C", "Y") == 2
+
+    # Test guessing outcome 'Z'
+    assert rock_paper_scissors.guess_outcome_score("A", "Z") == 3
+    assert rock_paper_scissors.guess_outcome_score("B", "Z") == 9
+    assert rock_paper_scissors.guess_outcome_score("C", "Z") == 6
 
 
 def test_calculate_rock_paper_scissors_score():
-    # Test input with a easy calorie file.
-    assert True
+    """Test the `calculate_rock_paper_scissors_score` function with a variety of input values."""
+    # Part 1
+    assert rock_paper_scissors.calculate_rock_paper_scissors_score(
+        "test/files/day2/input_easy.txt", "one") == TOTAL_SCORE_PART_1_EASY
+    assert rock_paper_scissors.calculate_rock_paper_scissors_score(
+        "test/files/day2/input.txt", "one") == TOTAL_SCORE_PART_1_HARD
 
-    # Test input with mian calorie file
-    assert True
+    # Part 2
+    assert rock_paper_scissors.calculate_rock_paper_scissors_score(
+        "test/files/day2/input_easy.txt", "two") == TOTAL_SCORE_PART_2_EASY
+    assert rock_paper_scissors.calculate_rock_paper_scissors_score(
+        "test/files/day2/input.txt", "two") == TOTAL_SCORE_PART_2_HARD
